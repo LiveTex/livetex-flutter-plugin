ea76cc0e9b1139ee8e73c873158e3081087a1165
jrip <mail@jrlab.ru>
2026-05-26 16:32:32 +0300

    up version

--- packages/livetex_chat/lib/src/livetex_trace.dart
+++ packages/livetex_chat/lib/src/livetex_trace.dart
@@ -1,4 +1,4 @@
-const String livetexChatPackageVersion = "0.6.1";
+const String livetexChatPackageVersion = "0.6.2";
 
 String livetexTraceFormatLine(String message) =>
     "${DateTime.now().toUtc().toIso8601String()} $message";
