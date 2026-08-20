2bb92f1dd8ca61e79eef89b3c92589afe256949d
zasert <slava.r@livetex.ru>
2026-08-21 00:22:17 +0300

    up version

--- packages/livetex_chat/lib/src/livetex_trace.dart
+++ packages/livetex_chat/lib/src/livetex_trace.dart
@@ -1,4 +1,4 @@
-const String livetexChatPackageVersion = "0.7.0";
+const String livetexChatPackageVersion = "0.8.0";
 
 String livetexTraceFormatLine(String message) =>
     "${DateTime.now().toUtc().toIso8601String()} $message";
